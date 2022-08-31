2063e25b77bfcd478c176cc1d94d3c3c3d39edc6
Evgenii Korolevskii <e-korolevskii@github.com>
2022-08-31 15:28:39 +0200

    add index.ts

--- index.js
+++ index.js
@@ -10,42 +10,24 @@ const json = XMLParser.parse(curContents, {ignoreAttributes: false});
 
 const sourceKeys = [];
 
-if (typeof json.configuration.packageSources != 'undefined') {
-  console.log(json.configuration.packageSources);
-  if (typeof json.configuration.packageSources.add != 'undefined') {
-        // file has at least one <add>
-        if (typeof json.configuration.packageSources.add[0] === 'undefined') {
-          // file has only one <add>
-          if (
-            json.configuration.packageSources.add['@_value']
-              .toLowerCase()
-              .includes(feedUrl.toLowerCase())
-          ) {
-            const key = json.configuration.packageSources.add['@_key'];
-            sourceKeys.push(key);
-          }
-        } else {
-          // file has 2+ <add>
-          for (
-            let i = 0;
-            i < json.configuration.packageSources.add.length;
-            i++
-          ) {
-            const source = json.configuration.packageSources.add[i];
-            const value = source['@_value'];
-            if (value.toLowerCase().includes(feedUrl.toLowerCase())) {
-              const key = source['@_key'];
-              sourceKeys.push(key);
-            }
-          }
-
-          // json.configuration.packageSources.add.forEach(source => {
-          //   const value = source['@_value'];
-          //   if (value.toLowerCase().includes(feedUrl.toLowerCase())) {
-          //     const key = source['@_key'];
-          //     sourceKeys.push(key);
-          //   }
-          // });
-        }
+if (typeof json.configuration?.packageSources?.add != 'undefined') {
+  if (Array.isArray(json.configuration.packageSources.add)) {
+    json.configuration.packageSources.add.forEach((source) => {
+      const value = source["@_value"];
+      if (value.toLowerCase().includes(feedUrl.toLowerCase())) {
+        const key = source["@_key"];
+        sourceKeys.push(key);
       }
+    });
+  } else {
+    if (
+      json.configuration.packageSources.add["@_value"]
+        .toLowerCase()
+        .includes(feedUrl.toLowerCase())
+    ) {
+      const key = json.configuration.packageSources.add["@_key"];
+      sourceKeys.push(key);
     }
+  }
+}
+
